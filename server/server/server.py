e69f26e17242e11b3a9372a99f082f4a74176b6a
Harrison Wong <harrison.c.wong@gmail.com>
2012-04-09 23:29:42 -0400

    Start security features on DHT

--- server/server/server.py
+++ server/server/server.py
@@ -1,5 +1,6 @@
 import threading
 import clr
+import hashlib
 
 # add ISIS
 clr.AddReference('isis2.dll')
@@ -16,10 +17,21 @@ print('Isis started')
 g = Group('FooBar')
 
 # Return true if a valid put, false otherwise
+# Check signature of message before putting into a restricted namespace
 def verifyPut(key, value):
-   return False
+   parts = str(key).split('/')
+   if parts[0] == 'keys':
+       return False #IMPLEMENT ME
+   elif parts[0] == 'files':
+       return False #IMPLEMENT ME
+   elif parts[0] == 'data':
+       hex = hashlib.sha1(value).hexdigest()
+       # Ensure that the location is a sha1 of the value
+       return parts[1] == hex
+   else:
+       return False
 g.DHTEnable(1, 1, 1, DHTVerifyPut(verifyPut)) # For debug only
-#g.DHTEnable(3, 6, 6, verifyPut) # For production testing
+#g.DHTEnable(3, 6, 6, DHTVerifyPut(verifyPut)) # For production testing
 
 
 def myfunc(i):
@@ -42,19 +54,33 @@ g.RegisterHandler(0, IsisDelegate[int](myfunc))
 g.RegisterHandler(1, IsisDelegate[float](myRfunc))
 g.RegisterViewHandler(ViewHandler(myViewFunc))
 g.Join()
-g.Send(0, 17)
-res = []
-nr = g.Query(Group.ALL, 1, 98.8, EOLMarker(), res);
-print('After Query got ' + nr.ToString() + ' results: ', res)
-res2 = []
-nr = g.Query(Group.ALL, 0, 98, EOLMarker(), res2);
-print('After Query got ' + nr.ToString() + ' results: ', res2)
+# g.Send(0, 17)
+# res = []
+# nr = g.Query(Group.ALL, 1, 98.8, EOLMarker(), res);
+# print('After Query got ' + nr.ToString() + ' results: ', res)
+# res2 = []
+# nr = g.Query(Group.ALL, 0, 98, EOLMarker(), res2);
+# print('After Query got ' + nr.ToString() + ' results: ', res2)
+
+
+def DHTGet(key):
+    x = g.DHTGet(key)
+    if x:
+        return x
+    else:
+        return None
 
 g.DHTPut("abc", "123")
-print g.DHTGet("abc")
+print DHTGet("abc")
+
+value = "blah blah"
+key = "data/"+hashlib.sha1(value).hexdigest()
+g.DHTPut(key, value)
+print DHTGet(key)
+
+
 
 t = threading.Thread(target=IsisSystem.WaitForever)
 t.daemon = True
 t.start()
-print('Hello world')
 raw_input('Press enter to quit')
